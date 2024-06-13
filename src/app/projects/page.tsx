884756713b723b4e97a4b593652242a29b45afda
Breno <eusoubrenovieira@gmail.com>
2024-06-13 15:58:37 -0300

    feat(page): new /projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,28 +1,48 @@
-'use client'
+import { Header } from '../_components/header'
+import { Heading } from '../_components/ui/heading'
+import { ProjectCard } from './components/project-card'
 
-import { Wrench } from 'lucide-react'
+const mockProjects = [
+  {
+    name: 'AskRooms',
+    description:
+      'A community-based platform for people to ask questions and share their thoughts',
+    images: ['/askrooms.png'],
+    githubUrl: null,
+    liveUrl: null,
+    tags: [
+      'NextJS',
+      'Tailwind',
+      'NextAuth',
+      'Prisma',
+      'Zod',
+      'React Query',
+      'React Hook Form',
+    ],
+  },
+]
 
 export default function ProjectsPage() {
   return (
-    <div className="flex h-screen items-center justify-center ">
-      <div className="flex flex-col aspect-square justify-center items-center gap-12 bg-accent px-4 py-1 rounded">
-        <div className="space-y-4">
-          <Wrench size={48} className="mx-auto" />
-          <h1 className="text-3xl font-bold text-center">
-            🛠️ Under construction
-          </h1>
-          <p className="max-w-[260px] mx-auto text-center">
-            The projects section is under construction. Check back later!
-          </p>
+    <div className="px-6">
+      <Header />
 
-          <button
-            className="bg-info block mx-auto font-bold text-foreground rounded px-4 py-2"
-            onClick={() => window.history.go(-1)}
-          >
-            Go back
-          </button>
+      <section>
+        <div className="mx-auto w-fit">
+          <Heading level={1} className="text-center">
+            Projects
+          </Heading>
+          <span className="text-muted mt-3 block text-center italic">
+            Projects I have worked on
+          </span>
         </div>
-      </div>
+
+        <div className="flex flex-col gap-8 max-w-3xl mx-auto my-16 md:[&>*:nth-child(odd)]:flex-row-reverse">
+          {mockProjects.map((project) => (
+            <ProjectCard key={project.name} {...project} />
+          ))}
+        </div>
+      </section>
     </div>
   )
 }
